374d60f6d56ba1b623b684b0999aa56abf248589
Alexis Hayat <alexis.hayat@cibiltech.com>
2022-07-14 22:46:46 +0200

    chore: remove jquery from mix

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Alexis Hayat - Portfolio</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js" defer></script>
 
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
